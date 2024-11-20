2c89e52bc2f9e3a12023c6126a6e55a649838f11
AbdoHamedd <abdohamed559@yahoo.com>
2024-11-20 23:07:57 +0200

    add the (go.sum) & (Application.go)

---
+++ Application.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type Application struct {
+	Gin *gin.Engine
+}
+
+func app() func() Application {
+	return func() Application {
+		var application Application
+		return application
+	}
+}
